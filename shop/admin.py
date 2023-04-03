ef09e1f713df039143336a8ad1436c85a00442ab
alexey <selex1995@yandex.ru>
2023-04-03 20:35:49 +0300

    register models in admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import Product, Category
+
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['category', 'name', 'slug', 'image_show', 'description', 'price', 'stock',
+                    'available', 'created', 'updated']
+    list_filter = ['created', 'updated', 'available']
+    list_editable = ['price', 'stock']
+    prepopulated_fields = {'slug': ('name',)}
+
+    def image_show(self, obj):
+        if obj.image:
+            return mark_safe("<img src='{}' width='60' />".format(obj.image.url))
+        return "None"
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
