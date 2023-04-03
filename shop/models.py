31f6ad90f8b80101de1602f576d396c90ed4369d
alexey <selex1995@yandex.ru>
2023-04-03 20:26:09 +0300

    add a Product model

--- shop/models.py
+++ shop/models.py
@@ -13,3 +13,25 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(to=Category, related_name='products', on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Изображение')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    stock = models.PositiveIntegerField(verbose_name='Остаток')
+    available = models.BooleanField(default=True, verbose_name='В продаже')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
