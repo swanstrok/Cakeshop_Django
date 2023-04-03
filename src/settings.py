e3739a004e4c5e115d84db6828a444eadf7eb661
alexey <selex1995@yandex.ru>
2023-04-03 20:09:28 +0300

    connect with PostgreSQL

--- src/settings.py
+++ src/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'EuphoriaShopDB',
+        'USER': 'admin',
+        'PASSWORD': '310242',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
