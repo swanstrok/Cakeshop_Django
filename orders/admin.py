f757e9b43e4ffb480b55520d4c9a5f08ab03f98e
alexey <selex1995@yandex.ru>
2023-04-04 18:34:50 +0300

    register order-models in orders/admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order, OrderProduct
+
 
 # Register your models here.
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+
+
+@admin.register(Order)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'surname', 'email', 'phone', 'address', 'created', 'is_paid']
+    list_filter = ['is_paid', 'created']
+    inlines = [OrderProductInline]
