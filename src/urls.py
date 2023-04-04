7594936890a832609b2fcb7f877e1ccec2bec3af
alexey <selex1995@yandex.ru>
2023-04-04 19:18:08 +0300

    change path to shop.urls

--- src/urls.py
+++ src/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls', namespace='shop')),
+    path('', include('shop.urls', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders'))
 ]
