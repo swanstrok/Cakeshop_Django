c0bd12724ae62b8c62d458297f23012a95700d88
alexey <selex1995@yandex.ru>
2023-04-04 18:33:33 +0300

    add OrderProduct-model in orders/models.py

--- orders/models.py
+++ orders/models.py
@@ -18,3 +18,14 @@ class Order(models.Model):
 
     def __str__(self):
         return f'Order №{self.id}'
+
+
+class OrderProduct(models.Model):
+    """Модель каждого продукта в заказе"""
+    order = models.ForeignKey(to='Order', related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, related_name='order_products', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.id}'
