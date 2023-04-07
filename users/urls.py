eefbb269e15ccc29ddd1b58438f6c73ad5729eb7
alexey <selex1995@yandex.ru>
2023-04-07 14:29:02 +0300

    add registration url and button in base.html

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-
+from .views import register
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),  # Стоковая аутентификация Django
+    path('', include('django.contrib.auth.urls')), # Стоковая аутентификация Django
+    path('register/', register, name='register'),
 
 ]
-
