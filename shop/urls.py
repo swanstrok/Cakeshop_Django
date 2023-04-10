309f295e0131287824700f343dc6a98835922fb1
alexey <selex1995@yandex.ru>
2023-04-10 21:08:44 +0300

    add ProductDetail view based in class

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,9 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', view=views.product_list, name='product_list'),
+    # path('', view=views.ProductList.as_view(), name='product_list'),
     path('<slug:category_slug>/', view=views.product_list, name='product_list_by_category'),
+    # path('<slug:category_slug>/', view=views.ProductList.as_view(), name='product_list_by_category')
     path('<int:id>/<slug:product_slug>/', view=views.product_detail, name='product_detail'),
 
 
