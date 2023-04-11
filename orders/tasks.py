b3dced1c67979c61af94954b4c2397c0407b5522
alexey <selex1995@yandex.ru>
2023-04-11 14:48:26 +0300

    add email notification for order

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,5 +1,9 @@
 from celery import shared_task
-from django.core.mail import send_mail
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from src.settings import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_HOST, EMAIL_PORT
 from .models import Order
 
 
@@ -7,12 +11,28 @@ from .models import Order
 def order_created(order_id):
     """Таска посылает уведомление на email, когда заказ успешно создан"""
     order = Order.objects.get(id=order_id)
-    subject = f"Заказ №{order.id}"
-    message = f"{order.name},\n\nВы успешно оформили заказ. Ваш номер заказа - {order.id}"
 
-    mail_sent = send_mail(subject=subject,
-                          message=message,
-                          from_email='admin@myshop.com',
-                          recipient_list=[order.email])
+    sender = EMAIL_HOST_USER
+    password = EMAIL_HOST_PASSWORD
+    server = smtplib.SMTP_SSL(f'{EMAIL_HOST}: {EMAIL_PORT}')
 
-    return mail_sent
+    msg = MIMEMultipart()
+    msg['Subject'] = f"Заказ №{order.id}"
+    msg['From'] = 'Euphoria Cakeshop <' + sender + '>'
+    msg['To'] = order.email
+    msg['Reply-To'] = sender
+
+    msg_body = MIMEText(f"{order.name},\n\nВы успешно оформили заказ. Ваш номер заказа - {order.id}",
+                        'plain')
+
+    msg.attach(msg_body)
+
+    try:
+        server.login(user=sender, password=password)
+        mail_sent = server.sendmail(from_addr=sender, to_addrs=order.email, msg=msg.as_string())
+        print('The message was sent successfully!')
+        return mail_sent
+    except Exception as e:
+        return f'{e}\nПроверьте ваш логин и пароль.'
+
+
