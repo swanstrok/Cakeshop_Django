ab7547507335a2f718cc80b08e050020fe0e97a3
alexey <selex1995@yandex.ru>
2023-04-18 11:00:33 +0300

    add comments to Celery.py

--- src/celery.py
+++ src/celery.py
@@ -2,6 +2,9 @@ import os
 from celery import Celery
 
 os.environ.setdefault(key='DJANGO_SETTINGS_MODULE', value='src.settings')
-app = Celery(main='src')
-app.config_from_object(obj='django.conf:settings', namespace='CELERY')
+app = Celery(main='src')  # указывается имя основного модуля в качестве аргумента
+
+app.config_from_object(obj='django.conf:settings',
+                       # Определяется файл настроек Django в качестве файла конфигурации для Celery
+                       namespace='CELERY')  # и предоставляется пространство имен "CELERY".
 app.autodiscover_tasks()
