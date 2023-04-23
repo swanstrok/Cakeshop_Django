1c033dd7863d1d1344b9c7073c29a9897422a623
alexey <selex1995@yandex.ru>
2023-04-23 15:54:09 +0300

    create apies package in shop-app, install DRF

--- src/settings.py
+++ src/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
     'users.apps.UsersConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,81 @@
 from django.test import TestCase
 
-import pytest
+from django.test import Client
+from http import HTTPStatus
 from django.contrib.auth.models import User
 
 
-# Create your tests here.
-@pytest.mark.django_db
-def test_user_create():
-    User.objects.create_user('user', 'user@mail.com', '333')
-    assert User.objects.count() == 1
+class UserTestCase(TestCase):
+    def setUp(self) -> None:
+        self.c = Client()
+
+    def test_is_ok_page_login(self):
+        response = self.c.get('/accounts/login/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_is_ok_page_register(self):
+        response = self.c.get('/accounts/register/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_login_user(self):
+        credentials = {
+            'username': 'Ivan Login',
+            'password': 'abrakadabra'
+        }
+        user = User.objects.create_user(**credentials)
+        response = self.c.post('/accounts/login/', credentials, follow=True)
+        self.assertTrue(response.context['user'].is_active)
+
+    def test_create_user(self):
+        user = User.objects.create_user(
+            username='TestIvan',
+            email='ivan@mail.com',
+            password='4ABefef3rgVM',
+        )
+        self.assertIsInstance(user, User)
+
+    def test_register_user(self):
+        data = {
+            'username': 'TestIvan',
+            'email': 'ivan@mail.com',
+            'password': '4ABefef3rgVM',
+            'password_confirmation': '4ABefef3rgVM',
+        }
+
+        response = self.c.post('/accounts/register/', data)
+
+        user = User.objects.create_user(
+            username=data['username'],
+            email=data['email'],
+            password=data['password']
+        )
+
+        user = User.objects.get(username=data['username'])
+
+        self.assertIsInstance(user, User)
+
+# import pytest
+# from django.contrib.auth.models import User
+# from django.urls import reverse
+#
+#
+# # Create your tests here.
+# @pytest.mark.django_db
+# def test_user_create():
+#     User.objects.create_user('user', 'user@mail.com', '333')
+#     assert User.objects.count() == 1
+#
+#
+#
+# @pytest.mark.django_db
+# def test_view_unauthorized(client):
+#     url = reverse('shop:product_list')
+#     response = client.get(url)
+#     assert response.status_code == 401
+#
+#
+# @pytest.mark.django_db
+# def test_view_as_admin(admin_client):
+#     url = reverse('shop:product_list')
+#     response = admin_client.get(url)
+#     assert response.status_code == 200
