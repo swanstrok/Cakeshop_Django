26bb22f16a302956772623a8a37c14f6e1386f81
alexey <selex1995@yandex.ru>
2023-04-25 21:12:57 +0300

    add simple-jwt urls to src/urls

--- src/urls.py
+++ src/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/drf-auth/', include('rest_framework.urls')),
     path('api/users/', include('users.api.urls')),
     path('api/orders/', include('orders.api.urls')),
